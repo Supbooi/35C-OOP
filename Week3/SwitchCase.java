19a691de5a426c19a793abe93fbea11738cc0e2e
aayush <subediaayush489@gmail.com>
2024-04-23 13:45:42 +0545

    edited

--- Week3/SwitchCase.java
+++ Week3/SwitchCase.java
@@ -3,8 +3,18 @@ package Week3;
 public class SwitchCase {
     public static void main(String[] args) {
         char value ='a';
+        int numVal = 10;
         switch (value) {
             case 'a':
+            // Nested Switch
+            switch(numVal){
+                case 10:
+                    System.out.println("10");
+                    break;
+                default:
+                    System.out.println("Default");
+                    break;
+            }
                 System.out.println("Apple");
                 break;
             case 'b':
